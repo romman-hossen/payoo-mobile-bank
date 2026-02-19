896ef1d97e7366845e8ba083046917941c463c2f
romman-hossen <mdromman417@gmail.com>
2026-02-19 15:53:50 +0600

    js function and home section added

--- script/script.js
+++ script/script.js
@@ -1,1 +1,23 @@
-console.log('connected')
+// console.log('connected')
+document.getElementById('login-btn').addEventListener('click',function(){
+    // 1.get the mobile number input
+    const numberInput = document.getElementById("input-number");
+    const contactNumber = numberInput.value;
+    console.log(contactNumber)
+    // 2.get the pin input 
+    const pinInput = document.getElementById("input-pin");
+    const pin = pinInput.value;
+    console.log(pin)
+    //3. match pin and mobile number 
+    if(contactNumber == "01760320585" && pin == "1234"){
+        // 3.1 :: true >> alert homepage
+        alert('login success');
+        // window.location.replace("/home.html")
+        window.location.assign("../home.html")
+    }
+    else{
+        // 3.1 :: false >> alert return
+        alert('login failed')
+        return;
+    }
+})
